86897c7bd8d48eb5d00e0a11a0fc1856209319cd
Robin <robin.pal@hotmail.de>
2025-01-06 23:22:31 +0100

    :construction: wip

--- letterui/views.py
+++ letterui/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .forms import ContactForm
 
+currentview = ''
+
 # Create your views here.
 def home(request):
-    return render(request, 'letterui/index.html')
+    global currentview
+    newview = request.GET.get('currentview', 'sender')
+    currentview = newview
+    return render(request, 'letterui/index.html', {'currentview': currentview})
 
 def contact_view(request):
     form = ContactForm()
@@ -13,6 +18,7 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             message = "Form submitted successfully!"
+            print(form.cleaned_data)
             form = ContactForm()  # Reset the form after submission
 
     return render(request, "letterui/contact.html", {"form": form, "message": message})
