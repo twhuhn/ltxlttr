bcd80ec8f54f9cc0c332e14af8ed046ee9544194
Robin <robin.pal@hotmail.de>
2025-01-09 00:20:50 +0100

    Add a clear button

--- letterui/views.py
+++ letterui/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, redirect
 from .forms import LatexLetterForm
 
 # Create your views here.
 def home(request):
     form = LatexLetterForm()
     message = ""
-
+    print(request)
     if request.method == "POST":
         form = LatexLetterForm(request.POST)
-        if form.is_valid():
+        valid = form.is_valid()
+        print(valid)
+        if valid:
             message = "Form submitted successfully!"
+            
             print(form.cleaned_data)
-            form = LatexLetterForm()  # Reset the form after submission
+
+            # Save the current content as a cookie
+            response = render(request, "letterui/index.html", {"form": form, "message": message})
+            response.set_cookie('content', json.dumps(form.cleaned_data))
+            return response
 
     return render(request, "letterui/index.html", {"form": form, "message": message})
+
+
+def clear(request):
+    response = redirect('/')
+    content_cookie = request.COOKIES.get('content')
+    if content_cookie:
+        response.delete_cookie('content')
+    
+    return response
